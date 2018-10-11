d0d34e7c65317fb736ba8275d388af7c583b2817
victorcezario97 <victorscezario@hotmail.com>
2018-10-11 02:56:14 -0300

    Initial commit

---
+++ src/Ex.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Ex {
+
+	public static void main(String[] args) {
+
+		int a;
+		double b;
+		
+		//testando o git
+	}
+
+}
